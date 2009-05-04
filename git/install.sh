0fdcf276419bd3c95fd25d48d3dd0d3aada7f0cc
Joe MacDonald <joe@deserted.net>
2009-05-04 08:40:13 -0400

    Adding gmail / imap support
    I do prefer to send my patches via gmail since that's typically where I
    any of my project-related email goes anyway and on at least some machines
    it saves me from having to set up an MTA.
    
    This also adds in a couple of reasonably essential options, too.  The
    default behaviour to not suppress cc-ing everyone in the signoff chain is
    just plain wrong, for example, and giggle is basically a less-ugly gitk.
    
    Signed-off-by: Joe MacDonald <joe@deserted.net>

--- git/install.sh
+++ git/install.sh
@@ -10,6 +10,7 @@ EOT
 fi
 
 cp gitconfig $HOME/.gitconfig
+chmod 600 $HOME/.gitconfig
 
 read -p "Full name:        " fullname
 read -p "email:            " emailaddr
@@ -19,3 +20,18 @@ sed -i "s/NAME/$fullname/g" $HOME/.gitconfig
 sed -i "s/SIGNKEY/$signkey/g" $HOME/.gitconfig
 sed -i "s/EMAIL/$emailaddr/g" $HOME/.gitconfig
 sed -i "s|HOMEDIR|$HOME|g" $HOME/.gitconfig
+
+cat <<EOT
+   If you have a GMail account and want to be able to use it to send your
+   patches out, this gitconfig includes an alias "git send-gmail".
+   Unfortunately it doesn't quite behave like "git send-email", this one does
+   the "git format-patch" for you and uploads the threaded patch set into your
+   Drafts folder so you can tweak it before sending it.
+EOT
+read junk
+
+read -p "GMail username:   " gmailuser
+read -p "GMail password:   " gmailpass
+
+sed -i "s/GMAIL_USERNAME/$gmailuser/g" $HOME/.gitconfig
+sed -i "s/GMAIL_PASSWORD/$gmailpass/g" $HOME/.gitconfig
