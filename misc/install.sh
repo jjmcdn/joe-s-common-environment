c84a4229b36257f556590f4e40eea7d6e7743e90
Joe MacDonald <joe.macdonald@windriver.com>
2009-05-12 08:15:42 -0400

    Adding dput.cf
    No magic in here, but I want everything I care about in one place so I
    don't have to remember what I've configured where.
    
    Signed-off-by: Joe MacDonald <joe@deserted.net>

---
+++ misc/install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# move the vimperatorrc into place, it's the least likely to collide.
+if [ -e $HOME/.vimperatorrc ]
+then
+   cat <<EOT
+   You already appear to have a $HOME/.vimperatorrc.  I'm going to move it to
+   the side for now, you can find it later as $HOME/.backup-vimperatorrc.
+EOT
+   mv $HOME/.vimperatorrc $HOME/.backup-vimperatorrc
+fi
+cp vimperatorrc $HOME/.vimperatorrc
+
+# Now do the dput.cf
+cat <<EOT
+   Now I'm going to add to (or possibly create) your ~/.dput.cf file so you
+   can use a short name to upload packages to your Launchpad.net PPA.  First
+   I'm going to ask you for a short name for your PPA, this will be what you
+   use on the command line, so choose something descriptive but easy to type.
+   You'll use it like:  'dput PPANAME'.
+
+   Secondly, I'll need your Launchpad.net username to figure out where your
+   incoming directory lives.
+EOT
+read -p "PPA Short Name:         "  ppaname
+read -p "Launchpad.net Username: "  ppausername
+TMPDIR=`mktemp -d`
+if [ $? -ne 0 ]
+then
+   cat <<EOT
+   Uh oh.  I tried to create a temporary directory for my scratch dput.cf file
+   but that failed for some reason.  Since I've already got the information
+   lying around, I've modified the local dput.cf file but
+
+      YOUR $HOME/.dput.cf HAS NOT BEEN UPDATED!
+
+   At this point your best option is probably to do something like this:
+
+      cat dput.cf.new >> $HOME/.dput.cf
+
+   but that's your call.
+EOT
+   cp dput.cf dput.cf.new
+   sed -i "s/PPANAME/$ppaname/g" ./dput.cf.new
+   sed -i "s/PPAUSERNAME/$ppausername/g" ./dput.cf.new
+else
+   # successfully created the temp directory.
+   cp dput.cf $TMPDIR
+   sed -i "s/PPANAME/$ppaname/g" $TMPDIR/dput.cf
+   sed -i "s/PPAUSERNAME/$ppausername/g" $TMPDIR/dput.cf
+   cat $TMPDIR/dput.cf >> $HOME/.dput.cf
+   rm -fr $TMPDIR
+fi
